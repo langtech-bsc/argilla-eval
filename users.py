6286879a9fdeee2b4c94cc0bfb062ca698f6840a
acumplid <alexandre.cumplido@bsc.es>
2024-08-23 11:19:04 +0200

    generate users dynamically

--- users.py
+++ users.py
@@ -1,11 +1,15 @@
 import argilla as rg
 from dotenv import load_dotenv
 import os
+import random
+import string
+import pandas as pd
 load_dotenv()
 
 ARGILLA_URL = os.getenv('API_URL')
 ARGILLA_KEY = os.getenv("API_KEY")
 EVAL_WORKSPACE = os.getenv("RG_WORKSPACE")
+NUM_USERS = int(os.getenv("NUMBER_USERS"))
 
 client = rg.Argilla(
     api_url=ARGILLA_URL,
@@ -14,24 +18,40 @@ client = rg.Argilla(
 
 users = client.users
 
-list_users = [
-    {"username": "username_a", "password":"password_a", "role":"annotator"},
-    {"username": "username_b", "password":"password_b", "role":"annotator"},
-    {"username": "username_c", "password":"password_c", "role":"annotator"},
-]
-
-# for user in list_users:
-#     rg.User(
-#         username=user['username'],
-#         password=user['password'],
-#         role=user['role'],
-#         client=client
-#     ).create().add_to_workspace(client.workspaces(EVAL_WORKSPACE))
+def generate_random_string(length=8):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
 
 
-# client.users('username_a').delete()
-# client.users('username_b').delete()
-# client.users('username_a').delete()
+def create_users(num_users, role="annotator"):
+    list_users = []
+    for _ in range(num_users):
+
+        username = generate_random_string(8)
+        password = generate_random_string(12)
+        list_users.append({
+            "username": username,
+            "password": password,
+            "role": role,
+        })
+    return list_users
+
+list_users = create_users(NUM_USERS)
+
+df_users = pd.DataFrame(list_users)
+df_users.to_csv('users.csv', index=False)
 
 
+for user in list_users:
+    rg.User(
+        username=user['username'],
+        password=user['password'],
+        role=user['role'],
+        client=client
+    ).create().add_to_workspace(client.workspaces(EVAL_WORKSPACE))
+
+
+# client.users('VgAGq0qY').delete()
+# client.users('5UGap1mL').delete()
+# client.users('L5q1gMX9').delete()
 for user in users: print(user)
